999b8e326bd3a1037a6170703208b2ed01577cb0
nileshgunjkar143 <33544329+nileshgunjkar143@users.noreply.github.com>
2022-01-21 18:05:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div id="main">
-      <h1 data-ns-test={'project-name'}>react</h1>
-      <p data-ns-test={'project-description'}>functional conponents</p>
+      <h1 data-ns-test={"project-name"}>react</h1>
+      <p data-ns-test={"project-description"}>functional conponents</p>
     </div>
   )
 }
